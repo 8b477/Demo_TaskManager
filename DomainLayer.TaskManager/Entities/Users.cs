6b14441c3198bc7289625afd3358babf5b30e723
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 11:08:40 +0100

    Create - User : Add Users entity

---
+++ DomainLayer.TaskManager/Entities/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.TaskManager.Entities
+{
+    public class Users
+    {
+        [Key]
+        public Guid Id_User { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        //public Roles Role { get; set; }
+    }
+}
