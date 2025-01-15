f97f56110b45ec6b9d5676ad363e5eb3f43fd8ff
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 11:10:44 +0100

    Create - Roles: Add Roles entity

---
+++ DomainLayer.TaskManager/Entities/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.TaskManager.Entities
+{
+    public class Roles
+    {
+        [Key]
+        public Guid Id_Role { get; set; }
+        public string RoleName { get; set; }
+    }
+}
