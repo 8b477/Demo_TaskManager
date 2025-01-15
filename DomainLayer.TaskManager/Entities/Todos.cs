23a1c59a392944e139b8931c335dfbfd02d60f84
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 11:20:14 +0100

    Create - Todos: Add Todos entity

---
+++ DomainLayer.TaskManager/Entities/Todos.cs
@@ -0,0 +1,17 @@
+﻿
+namespace DomainLayer.TaskManager.Entities
+{
+    public class Todos
+    {
+        public Guid Id_Todo { get; set; }
+        public string TodoName { get; set; }
+        public string TodoDescription { get; set; }
+        public DateTime TodoCreatedAt { get; set; }
+        public DateTime TodoUpdatedAt { get; set; }
+        public DateTime TodoFinishedAt { get; set; }
+        public bool TodoIsClosed { get; set; }
+        public bool TodoPriority { get; set; }
+        //public TodoStatus Status { get; set; }
+        public Users FK_User { get; set; }
+    }
+}
