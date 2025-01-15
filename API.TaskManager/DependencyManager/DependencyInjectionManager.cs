5205edaf956dc0bbdf02e76d4401b89e4496837c
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 21:31:55 +0100

    Config - programs.cs : Extend interface 'IServiceCollection' and add dependency between contract and repository

---
+++ API.TaskManager/DependencyManager/DependencyInjectionManager.cs
@@ -0,0 +1,25 @@
+﻿using DomainLayer.TaskManager.Interfaces;
+
+using InfrastructureLayer.TaskManager.Repositories;
+
+namespace API.TaskManager.DependencyManager
+{
+public static class DependencyInjectionManager
+    {
+        public static void AddDependency(this IServiceCollection service)
+        {
+            // Controller
+            service.AddControllers();
+
+            // Swagger
+            service.AddEndpointsApiExplorer();
+            service.AddSwaggerGen();
+
+            // Contract & Repository
+            service.AddScoped<ITodosRepository, TodoRepository>();
+            service.AddScoped<IUsersRepository, UserRepository>();
+            service.AddScoped<IRolesRepository, RoleRepository>();
+            service.AddScoped<ITodoStatusRepository, TodoStatusRepository>();
+        }
+    }
+}
