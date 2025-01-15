f06e9f2fc9ea4fcfbb276bb522fd2a5366de1e38
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 20:43:02 +0100

    Create - TodoStatusConfiguration : setup constraint for adding an object in DB with fluentAPI

---
+++ InfrastructureLayer.TaskManager/Context/Configurations/TodoStatusConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DomainLayer.TaskManager.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfrastructureLayer.TaskManager.Context.Configurations
+{
+    internal class TodoStatusConfiguration : IEntityTypeConfiguration<TodoStatus>
+    {
+        public void Configure(EntityTypeBuilder<TodoStatus> builder)
+        {
+            builder.HasKey(ts => ts.Id_TodoStatus);
+
+            builder.Property(ts => ts.TodoStatusName)
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
