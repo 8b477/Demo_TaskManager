b051bf0e45c54ed424c0ead5df87e7536910b9b8
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 20:44:19 +0100

    Create - UserConfiguration: setup constraint for adding an object in DB with fluentAPI

---
+++ InfrastructureLayer.TaskManager/Context/Configurations/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using DomainLayer.TaskManager.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace InfrastructureLayer.TaskManager.Context.Configurations
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+            builder.HasKey(u => u.Id_User);
+
+            builder.Property(u => u.UserName)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(u => u.Email)
+                .HasMaxLength(254)
+                .IsRequired();
+
+            builder.Property(u => u.PasswordHash)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
+
+            builder.HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.FK_Role)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
