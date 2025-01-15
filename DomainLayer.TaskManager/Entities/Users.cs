267a80c77678cfd0f0ad69094afa92c72aa58388
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 11:36:25 +0100

    Create - Users : Add in entity Users -> FK_Role type GUID

--- DomainLayer.TaskManager/Entities/Users.cs
+++ DomainLayer.TaskManager/Entities/Users.cs
@@ -14,6 +14,7 @@ namespace DomainLayer.TaskManager.Entities
         public string UserName { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
+        public Guid FK_Role { get; set; }
         public Roles Role { get; set; }
     }
 }
