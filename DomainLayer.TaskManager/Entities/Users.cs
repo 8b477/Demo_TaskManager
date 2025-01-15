f72cebad2f36dde53d4e3b9e205157ceb8d98c14
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 11:51:14 +0100

    Create - Users : Add navigation property -> ICollection<Todos> & remove useless 'using'

--- DomainLayer.TaskManager/Entities/Users.cs
+++ DomainLayer.TaskManager/Entities/Users.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace DomainLayer.TaskManager.Entities
 {
@@ -16,5 +12,6 @@ namespace DomainLayer.TaskManager.Entities
         public string PasswordHash { get; set; }
         public Guid FK_Role { get; set; }
         public Roles Role { get; set; }
+        public ICollection<Todos> Todos { get; set; }
     }
 }
