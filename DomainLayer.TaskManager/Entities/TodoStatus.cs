7b06069f40a6346552c21579e4d2df06722ed7c6
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 11:21:45 +0100

    Create - TodoStatus: Add TodoStatus entity

---
+++ DomainLayer.TaskManager/Entities/TodoStatus.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DomainLayer.TaskManager.Entities
+{
+    public class TodoStatus
+    {
+        public Guid Id_TodoStatus { get; set; }
+        public string TodoStatusName { get; set; }
+    }
+}
