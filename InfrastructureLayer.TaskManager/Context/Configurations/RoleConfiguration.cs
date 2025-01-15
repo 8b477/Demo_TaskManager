ae8ad885f57a1d00ce28728a25526c3636397e23
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 20:41:44 +0100

    Create - RoleConfiguration : setup constraint for adding an object in DB with fluentAPI

---
+++ InfrastructureLayer.TaskManager/Context/Configurations/RoleConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DomainLayer.TaskManager.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfrastructureLayer.TaskManager.Context.Configurations
+{
+    internal class RoleConfiguration : IEntityTypeConfiguration<Roles>
+    {
+        public void Configure(EntityTypeBuilder<Roles> builder)
+        {
+            builder.HasKey(r => r.Id_Role);
+
+            builder.Property(r => r.RoleName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.HasIndex(r => r.RoleName)
+                .IsUnique();
+        }
+    }
+}
