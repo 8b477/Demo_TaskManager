f9563479f74435bd4d63804f2ad4303229f73fb4
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 20:45:14 +0100

    Create - TodoConfiguration: setup constraint for adding an object in DB with fluentAPI

---
+++ InfrastructureLayer.TaskManager/Context/Configurations/TodoConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using DomainLayer.TaskManager.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace InfrastructureLayer.TaskManager.Context.Configurations
+{
+    internal class TodoConfiguration : IEntityTypeConfiguration<Todos>
+    {
+        public void Configure(EntityTypeBuilder<Todos> builder)
+        {
+            builder.HasKey(t => t.Id_Todo);
+
+            builder.Property(t => t.TodoName)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(t => t.TodoDescription)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder.Property(t => t.TodoCreatedAt)
+                .IsRequired();
+
+            builder.Property(t => t.TodoIsClosed)
+                .IsRequired();
+
+            builder.Property(t => t.TodoPriority)
+                .IsRequired();
+
+            builder.HasOne(t => t.User)
+                .WithMany(u => u.Todos)
+                .HasForeignKey(t => t.FK_User)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(t => t.Status)
+                .WithMany(ts => ts.Todos)
+                .HasForeignKey(t => t.FK_TodoStatus)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+    }
