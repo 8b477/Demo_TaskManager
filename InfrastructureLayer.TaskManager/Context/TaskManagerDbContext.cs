a83c4def1d77e039072c0be6e697fa3f1d9603e9
8b477 <jonathan_buchet@outlook.fr>
2025-01-15 20:38:32 +0100

    Create - TaskManagerContext : Creation of context for Ef core with entities Users, Roles, Todos, TodoStatus and intall package Microsoft.EntittyFrameworkCore V9.0.1

---
+++ InfrastructureLayer.TaskManager/Context/TaskManagerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DomainLayer.TaskManager.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace InfrastructureLayer.TaskManager.Context
+{
+    public class TaskManagerDbContext : DbContext
+    {
+        public TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options) : base(options) {}
+
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Todos> Todos { get; set; }
+        public DbSet<TodoStatus> TodoStatuses { get; set; }
+    }
+}
