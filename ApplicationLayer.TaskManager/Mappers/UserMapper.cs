379a508c66d959c43bd291a8578a73eac9b8f740
8b477 <jonathan_buchet@outlook.fr>
2025-01-18 10:41:57 +0100

    Create - Mapper : UserMapper DTO to Entiy

---
+++ ApplicationLayer.TaskManager/Mappers/UserMapper.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationLayer.TaskManager.DTOs;
+using DomainLayer.TaskManager.Entities;
+
+
+namespace ApplicationLayer.TaskManager.Mappers
+{
+    public static class UserMapper
+    {
+        public static Users ToEntity(this UserCreateDTO user)
+        {
+            return new Users
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                PasswordHash = user.Password
+            };
+        }
+    }
+}
