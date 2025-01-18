b5087be21098efff397dadcc3be4af3c62cc94a3
8b477 <jonathan_buchet@outlook.fr>
2025-01-18 15:58:37 +0100

    Create - ErrorResponse : Manager error service

---
+++ InfrastructureLayer.TaskManager/Errors/ErrorResponse.cs
@@ -0,0 +1,22 @@
+﻿
+namespace InfrastructureLayer.TaskManager.Errors
+{
+    internal static class ErrorResponse
+    {
+        internal static string NotFound()
+        {
+            return "the requested resource is not available";
+        }
+
+        internal static string BadRequest()
+        {
+            return "The request is not correct";
+        }
+
+        internal static string Unauthorized()
+        {
+            return "The request is not authorized";
+        }
+
+    }
+}
